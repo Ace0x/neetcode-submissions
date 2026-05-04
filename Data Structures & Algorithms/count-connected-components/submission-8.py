c574f40a354f468b554187ddfbe3b3ca43efcb96
Andrew Dunkerley <59582360+Ace0x@users.noreply.github.com>
2026-05-04 11:55:52 +0100

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        mp = defaultdict(list)
+        nodes = []
+        for edge in edges:
+            mp[edge[0]].append(edge[1])
+            mp[edge[1]].append(edge[0])
+            
+        for i in range(n):
+            nodes.append(i)
+
+        ctr = 0
+        visited = set()
+
+        def dfs(node):
+            
+            if node in visited:
+                return
+            #print(node," ")
+            visited.add(node)
+    
+            #print("here", mp[node])
+
+            for nod in mp[node]:
+                #print(nod)
+
+                dfs(nod)
+            return
+        #print(mp)
+        #dfs(list(nodes)[0])
+        #print(visited)
+        #print(nodes)
+        #print(mp.keys())
+       
+        for node in nodes:
+           
+            if node in visited:
+                continue
+            
+            #print(node)
+            #print(node)
+            dfs(node)
+            ctr += 1
+        #print(visited, ctr)
+        for i in nodes:
+            if i not in visited:
+                ctr += 1
+        return ctr
+
+        
