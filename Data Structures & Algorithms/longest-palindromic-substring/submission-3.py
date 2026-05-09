bd608226f7f148b29bc8a88b7141dc674b38b08c
Andrew Dunkerley <59582360+Ace0x@users.noreply.github.com>
2026-05-09 12:37:14 +0100

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,99 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        largest = ""
+
+        rlen  = 0
+
+        for i in range(len(s)):
+            l,r = i,i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r-l + 1 > rlen:
+                    largest = s[l:r+1]
+                    rlen = r - l + 1
+                l -= 1
+                r += 1
+            l,r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r-l + 1 > rlen:
+                    largest = s[l:r+1]
+                    rlen = r - l + 1
+                l -=1
+                r += 1
+        return largest
+        """start = 0
+        while start < len(s):
+            end = len(s) - 1
+
+            while end >= start:
+                l = start
+                r = end
+
+                while l <= r:
+                    if s[l] != s[r]:
+                        break
+                    l += 1
+                    r -= 1
+
+                # If we crossed, the whole substring was a palindrome
+                if l > r:
+                    candidate = s[start:end + 1]
+                    if len(candidate) > len(largest):
+                        largest = candidate
+                    break  # no need to try smaller endings for this start
+
+                end -= 1
+
+            start += 1
+
+        return largest"""
+
+
+"""class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+
+
+        word = {}
+        largest = ''
+        i = 0
+        while i < len(s):
+            ls = ''
+            rs = ''
+            tmp = i
+            j = len(s) - 1
+            while not i > j:
+                print(i,j)
+                if i == j:
+                    ls += s[i]
+                    if len(ls+rs) > len(largest):
+                        largest = ls+rs
+                    break
+                if i > j:
+                    break
+                left = s[i]
+                right = s[j]
+               
+                if left != right:
+                    ls = ''
+                    rs = ''
+                    i = tmp
+                    j -= 1
+                    continue
+                
+                
+                ls += s[i]
+                rs = s[j] + rs
+                print(ls,i,rs,j)
+                j -= 1
+                
+                i += 1
+
+            if i > j and len(ls+rs) > len(largest):
+                largest = ls+rs
+            
+            i = tmp + 1
+
+            
+            
+        return largest """
+
