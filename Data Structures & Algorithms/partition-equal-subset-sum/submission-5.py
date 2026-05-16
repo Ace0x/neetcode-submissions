b8e94208ab6cc492e23b9f113091ac2a8ba79b57
Andrew Dunkerley <59582360+Ace0x@users.noreply.github.com>
2026-05-16 10:45:34 +0100

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        memo = {}
+        
+        def cp(i,s1,s2):
+            if i >= len(nums):
+                return s1 == s2
+            if (i,s1) in memo:
+                return memo[(i,s1)]
+            
+
+            if i == len(nums):
+                return False
+
+            skip = cp(i+1,s1,s2+nums[i])
+            take = cp(i+1,s1+nums[i],s2)
+
+            memo[(i,s1)] = skip or take
+
+            return memo[(i,s1)]
+
+
+        return cp(0,0,0)
