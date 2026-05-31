0858acee873f4a158d4347f03b2c99619c442c3c
Andrew Dunkerley <59582360+Ace0x@users.noreply.github.com>
2026-05-31 11:28:30 +0100

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        n = len(s)
+
+        d = defaultdict(list)
+
+        for i in range(n):
+            d[s[i]].append(i)
+
+        l = 0
+        last = 0
+        res = []
+        for c in range(n):
+            cur = d[s[c]][-1]
+            if cur > last:
+                last = cur
+            if c == last:
+                res.append(c + 1 - l)
+                l = c + 1
+        print(res)
+        return res
